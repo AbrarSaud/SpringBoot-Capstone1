e4e1db4b6876822ccd8a38e8d7d88339b0ed8cda
AbrarSaud <abrar.sauod0@gmail.com>
2025-03-17 03:44:53 +0300

    Added logic and business logic for MerchantStock

--- src/main/java/com/example/capstone1/Controller/MerchantStockController.java
+++ src/main/java/com/example/capstone1/Controller/MerchantStockController.java
@@ -1,11 +1,68 @@
 package com.example.capstone1.Controller;
 
 
+import com.example.capstone1.Api.ApiResponse;
+import com.example.capstone1.Model.MerchantStock;
+import com.example.capstone1.Service.MerchantStockService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api/v1/souky-store/merchant-stock")
+@RequiredArgsConstructor
 public class MerchantStockController {
+    private final MerchantStockService merchantStockService;
 
     // Getting IN Controller
+    @GetMapping("/get")
+    public ResponseEntity<?> getAllMerchantStocks() {
+        return ResponseEntity.ok(merchantStockService.getAllMerchantStocks());
+    }
+
     // Adding IN Controller
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addMerchantStock(@RequestBody @Valid MerchantStock merchantStock, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().body(new ApiResponse(errors.getFieldError().getDefaultMessage()));
+        }
+
+        int result = merchantStockService.addMerchantStock(merchantStock);
+
+        if (result == 200) {
+            return ResponseEntity.ok(new ApiResponse("Merchant stock added successfully!"));
+        } else if (result == 1) {
+            return ResponseEntity.status(404).body(new ApiResponse("Merchant stock ID already exists!"));
+        } else if (result == 2) {
+            return ResponseEntity.status(404).body(new ApiResponse("Invalid Merchant ID!"));
+        }
+
+        return ResponseEntity.status(500).body(new ApiResponse("Unexpected error!"));
+    }
+
     // Updating IN Controller
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateMerchantStock(@PathVariable String id, @RequestBody @Valid MerchantStock merchantStock, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().body(new ApiResponse(errors.getFieldError().getDefaultMessage()));
+        }
+        boolean isUpdated = merchantStockService.updateMerchantStock(id, merchantStock);
+
+        if (isUpdated) {
+            return ResponseEntity.ok(new ApiResponse("Merchant stock updated successfully!"));
+        }
+        return ResponseEntity.status(404).body(new ApiResponse("Merchant stock not found!"));
+    }
+
     // Deleting IN Controller
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteMerchantStock(@PathVariable String id) {
+        boolean idDeleted = merchantStockService.deleteMerchantStock(id);
+        if (idDeleted) {
+            return ResponseEntity.ok(new ApiResponse("Merchant stock deleted successfully!"));
+        }
+        return ResponseEntity.status(404).body(new ApiResponse("Merchant stock not found!"));
+    }
 }
--- src/main/java/com/example/capstone1/Service/MerchantStockService.java
+++ src/main/java/com/example/capstone1/Service/MerchantStockService.java
@@ -5,9 +5,60 @@ import com.example.capstone1.Model.MerchantStock;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-// MerchantStockController
+
 @Service
 public class MerchantStockService {
     private ArrayList<MerchantStock> merchantStocks = new ArrayList<>();
+    private MerchantService merchantService;
 
+    // Getting IN Service
+    public ArrayList<MerchantStock> getAllMerchantStocks() {
+        return merchantStocks;
+    }
+
+    // Adding IN Service
+    public int addMerchantStock(MerchantStock merchantStock) {
+        boolean merchantExists = false;
+        for (Merchant m : merchantService.getAllMerchants()) {
+            if (m.getId().equals(merchantStock.getMerchantID())) {
+                merchantExists = true;
+                break;
+            }
+        }
+        if (!merchantExists) {
+            return 2;
+        }
+
+        for (MerchantStock ms : merchantStocks) {
+            if (ms.getId().equals(merchantStock.getId())) {
+                return 1;
+            }
+        }
+
+
+        merchantStocks.add(merchantStock);
+        return 200;
+    }
+
+    // Updating IN Service
+    public boolean updateMerchantStock(String id, MerchantStock merchantStock) {
+        for (int i = 0; i < merchantStocks.size(); i++) {
+            if (merchantStocks.get(i).getId().equals(id)) {
+                merchantStocks.set(i, merchantStock);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Deleting IN Service
+    public boolean deleteMerchantStock(String id) {
+        for (int i = 0; i < merchantStocks.size(); i++) {
+            if (merchantStocks.get(i).getId().equals(id)) {
+                merchantStocks.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
 }
